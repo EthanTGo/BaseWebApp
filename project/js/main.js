669f9b3e02f8443a4c72aa582e23a14e822dab5f
EthanTGo <ethango@bu.edu>
2019-10-15 14:49:41 -0400

    Integrated basic web API

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,30 @@
+
+
+
+function getWeather(searchQuery){
+	var url = "https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=imperial&APPID="+apiKey;
+
+
+	// This part resets the entry whenever we click a search button
+	$(".city").text("");
+	$(".temp").text("");
+	$(".error-message").text("");
+
+	//IF success, do this, else IF fail display error message
+	// Jquert $ targets HTMl elements with a certain class
+	$.ajax(url,{success: function(data){
+		$(".city").text(data.name);
+		$(".temp").text(data.main.temp);
+	}, error: function(error){
+		$(".error-message").text("An error occured");
+	}})
+}
+
+function searchWeather(){
+	var searchQuery = $(".search").val();
+	getWeather(searchQuery);
+}
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
